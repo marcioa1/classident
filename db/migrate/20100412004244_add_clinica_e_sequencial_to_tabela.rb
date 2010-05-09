cb72007b251abff48a041a30c113f27a6d0eabee
marcioa1 <marcioa1@gmail.com>
2010-05-08 22:28:57 -0300

    novas migracoes

--- db/migrate/20100412004244_add_clinica_e_sequencial_to_tabela.rb
+++ db/migrate/20100412004244_add_clinica_e_sequencial_to_tabela.rb
@@ -1,12 +1,14 @@
 class AddClinicaESequencialToTabela < ActiveRecord::Migration
   def self.up
     add_column :tabelas, :clinica, :string
+    add_column :tabelas, :clinica_id, :integer
     add_column :tabelas, :sequencial, :integer
     add_column :item_tabelas, :sequencial, :integer
     add_column :item_tabelas, :clinica, :string
   end
 
   def self.down
+    remove_column :tabelas, :clinica_id
     remove_column :item_tabelas, :clinica
     remove_column :item_tabelas, :sequencial
     remove_column :tabelas, :sequencial
