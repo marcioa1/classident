91302b9dede5fbc3c801b0bb44c13e6a8ff53ed2
marcioa1 <marcioa1@gmail.com>
2011-02-28 13:23:10 -0300

    consertos protéticos?

--- db/migrate/20091213141837_add_sequencial_to_pagamento.rb
+++ db/migrate/20091213141837_add_sequencial_to_pagamento.rb
@@ -1,9 +1,9 @@
 class AddSequencialToPagamento < ActiveRecord::Migration
   def self.up
     add_column :pagamentos, :sequencial, :integer
-    add_column :pagamentos, :valor_terceiros, :decimal, :precision=>9, :scale=>2
-    add_column :pagamentos, :valor_cheque, :decimal, :precision=>9, :scale=>2
-    add_column :pagamentos, :valor_restante, :decimal, :precision=>9, :scale=>2
+    add_column :pagamentos, :valor_terceiros, :decimal, :precision=>9, :scale=>2, :default => 0
+    add_column :pagamentos, :valor_cheque, :decimal, :precision=>9, :scale=>2, :default => 0
+    add_column :pagamentos, :valor_restante, :decimal, :precision=>9, :scale=>2, :default => 0
     add_column :pagamentos, :opcao_restante, :integer
     add_column :pagamentos, :conta_bancaria_id, :integer
     add_column :pagamentos, :numero_do_cheque, :string, :limit=>20
