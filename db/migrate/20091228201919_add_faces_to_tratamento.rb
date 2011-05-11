6ea7e026c7fe1f27102eb32f5fc8351843ec279a
marcioa1 <marcioa1@gmail.com>
2011-05-11 14:03:23 -0300

    face palatina

--- db/migrate/20091228201919_add_faces_to_tratamento.rb
+++ db/migrate/20091228201919_add_faces_to_tratamento.rb
@@ -3,9 +3,11 @@ class AddFacesToTratamento < ActiveRecord::Migration
     add_column :tratamentos, :mesial, :boolean
     add_column :tratamentos, :oclusal, :boolean
     add_column :tratamentos, :distal, :boolean
+    add_column :tratamentos, :palatina, :boolean
   end
 
   def self.down
+    remoce_column :tratamentos, :palatina
     remove_column :tratamentos, :distal
     remove_column :tratamentos, :oclusal
     remove_column :tratamentos, :mesial
