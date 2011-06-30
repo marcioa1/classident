b98358124cbfb2e69b5998869564a58b5b2e7a7b
marcioa1 <marcioa1@gmail.com>
2011-06-30 12:52:14 -0300

    upcase no logradouro antes da busca pelo cep

--- app/controllers/ceps_controller.rb
+++ app/controllers/ceps_controller.rb
@@ -1,15 +1,12 @@
 class CepsController < ApplicationController
 
   def busca_pelo_logradouro
+    logradouro = params[:logradouro].upcase.strip  + '%'
     cep = Cep.find(:all, 
-                   :conditions=>['logradouro LIKE ?', params[:logradouro].strip! + '%' ],
+                   :conditions=>['logradouro LIKE ?', logradouro ],
                    :order => 'cidade, bairro'
     )
-    # if cep.size == 1
-      # render :json => cep.first.cep.to_json
-    # else
-      render :partial => '/ceps/ceps', :locals => {:ceps => cep}
-    # end
+    render :partial => '/ceps/ceps', :locals => {:ceps => cep}
   end
 
 end
