06ac2e051257771d65e96847b77f690da502584b
marcioa1 <marcioa1@gmail.com>
2011-09-06 12:26:51 -0300

    inclusão de senhas

--- app/controllers/clinicas_controller.rb
+++ app/controllers/clinicas_controller.rb
@@ -1,6 +1,7 @@
 class ClinicasController < ApplicationController
   
   layout "adm"
+  before_filter :require_master_user, :only =>[:relatorio_de_exclusao]
   
   def selecionou_clinica
     session[:clinica_id] = params[:clinica_id]
