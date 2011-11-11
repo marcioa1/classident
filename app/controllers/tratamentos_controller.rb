e19ee7110e3f81cb8d58da86eb9ddb71439727b1
marcioa1 <marcioa1@gmail.com>
2011-11-11 14:37:06 -0200

    coloquei clinica_id na criação de débito qundo sala trtamento com data detnro do formulário, e não pelo link de finalizar

--- app/controllers/tratamentos_controller.rb
+++ app/controllers/tratamentos_controller.rb
@@ -71,11 +71,12 @@ class TratamentosController < ApplicationController
           @debito = Debito.find_by_tratamento_id(@tratamento.id)
           if @debito.nil?
             @debito = Debito.new
-            @debito.paciente_id = @tratamento.paciente_id
+            @debito.paciente_id   = @tratamento.paciente_id
             @debito.tratamento_id = @tratamento.id
+            @debito.clinica_id    = @tratamento.paciente.clinica_id
           end
           @debito.descricao = @tratamento.descricao
-          @debito.valor     = @tratamento.valor
+          @debito.valor     = @tratamento.valor_com_desconto
           @debito.data      = @tratamento.data
           @debito.save
         end
