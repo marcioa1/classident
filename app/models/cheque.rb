661704c3d8056e9c7d2b492c9826840ecd77978e
marcioa1 <marcioa1@gmail.com>
2011-12-12 10:38:49 -0200

    acompanhamentos em cheques estva trazendo user como nulo

--- app/models/cheque.rb
+++ app/models/cheque.rb
@@ -301,7 +301,7 @@ class Cheque < ActiveRecord::Base
   def acompanhamentos
     result = ''
     self.acompanhamento_cheques.each do |a|
-      result += a.updated_at.to_s_br + " ( #{a.user.login} ) , " + a.descricao + "</br>"
+      result += a.updated_at.to_s_br + " ( #{a.user && a.user.login} ) , " + a.descricao + "</br>"
     end
     result
   end
