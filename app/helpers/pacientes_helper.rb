51831b4fb308972b85741400a2a1cd165de8c822
marcioa1 <marcioa1@gmail.com>
2011-12-27 10:04:53 -0200

    relatorio de exclusao quando o usuario já foi excluido

--- app/helpers/pacientes_helper.rb
+++ app/helpers/pacientes_helper.rb
@@ -1,7 +1,7 @@
 module PacientesHelper
   
   def nome_paciente(paciente)
-     "<p class='identifica_paciente'>Ficha do paciente (#{paciente.codigo}) : <b><a href='/pacientes/#{paciente.id}/abre'>#{paciente.nome}</a></b> &nbsp;&nbsp;&nbsp;&nbsp; ( Tel.: #{paciente.telefones} , clínica: #{paciente.clinica.sigla})</p> " if paciente
+     "<p class='identifica_paciente'>Ficha do paciente (#{paciente.codigo}) : <b><a href='/pacientes/#{paciente.id}/abre'>#{paciente.nome}</a></b> &nbsp;&nbsp;&nbsp;&nbsp; ( Tel.: #{paciente.telefones} , clínica: #{paciente.clinica.sigla})  saldo : #{paciente.saldo.real} </p>" if paciente
   end
   
 end
